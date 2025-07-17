d258b4e066d449fdf32e82dae468b28141410917
pranav reddy <saipranav919@gmail.com>
2025-07-17 17:46:26 +0100

    Initial Commit

---
+++ accountability/accountabilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  accountabilityApp.swift
+//  accountability
+//
+//  Created by Pranav reddy on 17/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct accountabilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ accountabilityTests/accountabilityTests.swift
@@ -0,0 +1,17 @@
+//
+//  accountabilityTests.swift
+//  accountabilityTests
+//
+//  Created by Pranav reddy on 17/07/2025.
+//
+
+import Testing
+@testable import accountability
+
+struct accountabilityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
